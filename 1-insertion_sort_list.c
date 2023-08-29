98720dcacf9262de8bbe36d74da799d779358168
Chris-Effah <akboateng41@gmail.com>
2023-08-28 18:43:25 -0700

    done

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,62 +1,55 @@
 #include "sort.h"
 
 /**
- * swap_nodes - swaps two nodes in a doubly linked list
- * @list: double pointer to the head of the list
+ * swap_nodes - swap two nodes in a doubly linked list
+ * @list: list of integers
  * @node1: first node
  * @node2: second node
  */
 void swap_nodes(listint_t **list, listint_t *node1, listint_t *node2)
 {
-	if (node1->prev != NULL)
-	{
-		node1->prev->next = node2;
-	}
+	if (node1->prev)
+	node1->prev->next = node2;
 	else
 		*list = node2;
 
-	if (node2->next != NULL)
+	if (node2->next)
 		node2->next->prev = node1;
 
 	node1->next = node2->next;
 	node2->prev = node1->prev;
-
 	node2->next = node1;
+
+	if (node1->prev)
+		node1->prev->next = node2;
+	else
+		node2->prev = NULL;
+
 	node1->prev = node2;
 }
 
-
 /**
- * insertion_sort_list - a function that sorts a doubly linked
- * list of integers in ascending order using the Insertion sort algorithm
- * @list: list of intergers to be sorted
- * Return: Void
- **/
+ * insertion_sort_list - sorts a doubly linked
+ * list of integers in ascending order
+ * using the Insertion sort algorithm
+ * @list: double pointer to the head of the list
+ */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *current;
-	listint_t *insertion_point;
-	listint_t *temp;
+	listint_t *i, *insert, *temp;
 
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
-	current = (*list)->next;
-
-	while (current != NULL)
+	for (i = (*list)->next; i != NULL; i = temp)
 	{
-		insertion_point = current->prev;
-		temp = current;
-
-		while (insertion_point != NULL && insertion_point->n > temp->n)
+		temp = i->next;
+		insert = i->prev;
+		while (insert != NULL && i->n < insert->n)
 		{
-			swap_nodes(list, insertion_point, temp);
-			insertion_point = temp->prev;
+			swap_nodes(list, insert, i);
 			print_list(*list);
-
-			temp = insertion_point;
-
+			insert = i->prev;
 		}
-		current = current->next;
 	}
 }
