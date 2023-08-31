552a4f127a6c6f99b51acaf03772375a03e37807
Chris-Effah <akboateng41@gmail.com>
2023-08-30 22:54:18 -0700

    task 1

---
+++ 102-counting_sort.c
@@ -0,0 +1,17 @@
+#include "sort.h"
+
+/**
+ * counting_sort -  a function that sorts an array of integers
+ * in ascending order using the Counting sort algorithm
+ * @array: array of integers
+ * @size: size of array
+ **/
+void counting_sort(int *array, size_t size)
+{
+	if (size <= 1)
+	{
+		return;
+	}
+
+
+}
