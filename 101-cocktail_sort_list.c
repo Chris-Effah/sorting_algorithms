8cfa30f558548393e4bba504cb8c7362057011a1
Chris-Effah <akboateng41@gmail.com>
2023-09-02 07:26:22 -0700

    cocktail shaker

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -1,93 +1,70 @@
 #include "sort.h"
 
 /**
-  * swap_node_ahead - Swaps a node in a listint_t doubly-linked list
-  * of integers with the node ahead of it.
-  * @list: A pointer to the head of a doubly-linked list
-  * @tail: A pointer to the tail of the doubly-linked list.
-  * @shaker: A pointer to the current swapping node of the cocktail shaker
-  */
-void swap_node_ahead(listint_t **list, listint_t **tail, listint_t **shaker)
+ * swap_nodes - Swaps a node in a listint_t doubly-linked list
+ * of integers with the node ahead of it.
+ * @list: A pointer to the head of a doubly-linked list
+ * @node1: A pointer to the tail of the doubly-linked list.
+ * @node2: A pointer to the current swapping node of the cocktail shaker
+ */
+void swap_nodes(listint_t **list, listint_t *node1, listint_t *node2) 
 {
-	listint_t *temp = (*shaker)->next;
+	if (node1->prev != NULL)
+		node1->prev->next = node2;
+	if (node2->next != NULL)
+		node2->next->prev = node1;
+	node1->next = node2->next;
+	node2->prev = node1->prev;
+	node1->prev = node2;
+	node2->next = node1;
 
-	if ((*shaker)->prev != NULL)
-		(*shaker)->prev->next = temp;
-	else
-		*list = temp;
-	temp->prev = (*shaker)->prev;
-	(*shaker)->next = temp->next;
-	if (temp->next != NULL)
-		temp->next->prev = *shaker;
-	else
-		*tail = *shaker;
-	(*shaker)->prev = temp;
-	temp->next = *shaker;
-	*shaker = temp;
+	if (node1->prev == NULL)
+		*list = node1;
+	if (node2->next == NULL)
+		*list = node2;
 }
 
 /**
-  * swap_node_behind - Swaps a node in a listint_t doubly-linked list
-  * of integers with the node behind it.
-  * @list: A pointer to the head of a doubly-linked list
-  * @tail: A pointer to the tail of the doubly-linked list
-  * @shaker: A pointer to the current swapping node of the cocktail shaker
-  */
-void swap_node_behind(listint_t **list, listint_t **tail, listint_t **shaker)
-{
-	listint_t *temp = (*shaker)->prev;
-
-	if ((*shaker)->next != NULL)
-		(*shaker)->next->prev = temp;
-	else
-		*tail = temp;
-	temp->next = (*shaker)->next;
-	(*shaker)->prev = temp->prev;
-	if (temp->prev != NULL)
-		temp->prev->next = *shaker;
-	else
-		*list = *shaker;
-	(*shaker)->next = temp;
-	temp->prev = *shaker;
-	*shaker = temp;
-}
-
-/**
-  * cocktail_sort_list - Sorts a listint_t doubly-linked list of integers
-  * @list: A pointer to the head of the listint_t doubly-linked list
-  */
+ * cocktail_sort_list - Sorts a listint_t doubly-linked list of integers
+ * @list: A pointer to the head of the listint_t doubly-linked list
+ */
 void cocktail_sort_list(listint_t **list)
 {
-	listint_t *tail, *shaker;
-	bool shaken = false;
-
-	if (list == NULL || *list == NULL || (*list)->next == NULL)
+	int swapped;
+	listint_t *p1, *p2, *end = NULL;
+	if (list == NULL || *list == NULL)
 		return;
-
-	for (tail = *list; tail->next != NULL;)
-		tail = tail->next;
-
-	while (shaken == false)
+	do 
 	{
-		shaken = true;
-		for (shaker = *list; shaker != tail; shaker = shaker->next)
+		swapped = 0;
+		p1 = *list;
+		while (p1->next != end) 
 		{
-			if (shaker->n > shaker->next->n)
+			if (p1->n > p1->next->n) 
 			{
-				swap_node_ahead(list, &tail, &shaker);
-				print_list((const listint_t *)*list);
-				shaken = false;
+				swap_nodes(list, p1, p1->next);
+				print_list(*list);
+				swapped = 1;
 			}
+			else
+				p1 = p1->next;
 		}
-		for (shaker = shaker->prev; shaker != *list;
-				shaker = shaker->prev)
+		if (swapped == 0)
+			break;
+		swapped = 0;
+		p2 = p1;
+		while (p2->prev != *list) 
 		{
-			if (shaker->n < shaker->prev->n)
+			if (p2->n < p2->prev->n) 
 			{
-				swap_node_behind(list, &tail, &shaker);
-				print_list((const listint_t *)*list);
-				shaken = false;
+				swap_nodes(list, p2->prev, p2);
+				print_list(*list);
+				swapped = 1;
 			}
+			else
+				p2 = p2->prev;
 		}
+		end = p2;
 	}
+	while (swapped);
 }
--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -8,10 +8,44 @@
  **/
 void counting_sort(int *array, size_t size)
 {
+	int max_val;
+	int *count_array, i, *output_array, output_index;
+
 	if (size <= 1)
 	{
 		return;
 	}
 
+	max_val = array[0];
+	for (i = 1; i < size; i++)
+	{
+		if (array[i] > max_val)
+		{
+			max_val = array[i];
+		}
 
+	}
+	count_array = calloc(max_val + 1, sizeof(int));
+
+	for (i = 0; i < (int)size; i++)
+	{
+		count_array[arr[i]]++;
+	}
+	output_array = malloc(size * sizeof(int));
+
+	output_index = 0;
+	for (i = 0, i <= max_val; i++)
+	{
+		while (count_array[i] > 0)
+		{
+			output_array[output_index] = i;
+			output_index++;
+			count_array[i]--;
+		}
+
+	}
+	for (i = 0; i < (int)size; i++)
+		arr[i] = output_array[i];
+	free(count_array);
+	free(output_array);
 }
